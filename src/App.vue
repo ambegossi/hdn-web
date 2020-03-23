32371f0423dfc107815d1509983c8f51c67cbb8c
Anderson Begossi <andersonbegossi@gmail.com>
2020-03-23 20:41:23 -0300

    Added general styles

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav"><router-link to="/">Home</router-link> |</div>
-    <router-view />
+    <Header />
   </div>
 </template>
 
-<style></style>
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  padding: 0px;
+  margin: 0px;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  color: #6d6868;
+  background: #fff;
+}
+
+a {
+  color: #6d6868;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+</style>
