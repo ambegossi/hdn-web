cb3ce455db3f90bff52c1d69470007e3a24fd961
Anderson Begossi <andersonbegossi@gmail.com>
2020-03-23 20:28:45 -0300

    Initial setup w/ ESLint, Prettier and EditorConfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,14 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
+    'prettier'
   ],
   parserOptions: {
     parser: 'babel-eslint',
   },
+  plugins: ['prettier'],
   rules: {
+    'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
