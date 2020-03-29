e8d14580ca3a40a65a7a8a42f6430b12823e6c4a
Anderson Begossi <andersonbegossi@gmail.com>
2020-03-29 14:27:17 -0300

    Updated API baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://my-json-server.typicode.com/ambegossi/hdn-api',
 });
 
 export default api;
